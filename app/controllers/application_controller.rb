9735c011f0a12000865449da5ecb448764be66ce
EfremL <efremlehman@icloud.com>
2026-04-15 16:05:17 -0400

    Created base html and css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def logged_in_user?
+    !session[:user_id].nil?
+  end
+
+  # Function to block non-logged in users from accessing a page
+  def authorized
+    unless logged_in_user?
+      flash[:alert] = "You must be logged in to access this page"
+      #redirect_to whatever_login_page_is_defined_as
+    end
+  end
 end
