3d7e9c05627e22762d4fbe247ef2f56a46ac6e42
EfremL <efremlehman@icloud.com>
2026-04-15 17:31:31 -0400

    Incorporated sign up and log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_15_192449) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_14_174158) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
