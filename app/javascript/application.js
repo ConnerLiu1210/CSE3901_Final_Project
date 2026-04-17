09b7753eda44c569b1da8c4891ae1c66b8a21e8f
EfremL <efremlehman@icloud.com>
2026-04-17 16:17:33 -0400

    Updated flash message timing and animation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+// import "controllers"
+
+// Makes flash messages disappear after 2 seconds
+const flashAutoDisappear = () => {
+    const flashes = document.querySelectorAll('.flash');
+
+    flashes.forEach(flash => {
+        setTimeout(() => {
+            flash.style.opacity = '0';
+            flash.style.transform = 'translateY(-10px)';
+
+            setTimeout(() => {
+                flash.remove();
+            }, 600);
+        }, 2000);
+    });
+};
+// The event listener for flashAutoDisappear
+document.addEventListener("DOMContentLoaded", flashAutoDisappear);
